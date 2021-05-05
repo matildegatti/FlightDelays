e96f0345ce4aeae11f40b4994da5ccc4298080d9
monge <alberto.monge@polito.it>
2021-05-05 15:35:04 +0200

    Soluzione svolta in video-lezione

--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -1,5 +1,149 @@
 package it.polito.tdp.extflightdelays.model;
 
-public class Model {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import it.polito.tdp.extflightdelays.db.ExtFlightDelaysDAO;
+
+public class Model {
+	
+	private SimpleWeightedGraph<Airport, DefaultWeightedEdge> grafo;
+	private ExtFlightDelaysDAO dao;
+	private Map<Integer,Airport> idMap;
+	private Map<Airport,Airport> visita;
+	
+	public Model() {
+		dao = new ExtFlightDelaysDAO();
+		idMap = new HashMap<Integer,Airport>();
+		dao.loadAllAirports(idMap);
+	}
+	
+	public void creaGrafo(int x) {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		//aggiungo vertici "filtrati"
+		Graphs.addAllVertices(grafo, dao.getVertici(x, idMap));
+		
+		//aggiungo gli archi
+		for(Rotta r : dao.getRotte(idMap)) {
+			if(this.grafo.containsVertex(r.getA1()) && 
+					this.grafo.containsVertex(r.getA2())) {
+				DefaultWeightedEdge e = this.grafo.getEdge(r.getA1(), r.getA2());
+				if(e == null) {
+					Graphs.addEdgeWithVertices(grafo, r.getA1(), r.getA2(), r.getN());
+				} else{
+					double pesoVecchio = this.grafo.getEdgeWeight(e);
+					double pesoNuovo = pesoVecchio + r.getN();
+					this.grafo.setEdgeWeight(e, pesoNuovo);
+				}
+			}
+		}
+		
+	}
+
+	public Set<Airport> getVertici() {
+		if(grafo != null)
+			return grafo.vertexSet();
+		
+		return null;
+	}
+	
+	public int getNVertici() {
+		if(grafo != null)
+			return grafo.vertexSet().size();
+		
+		return 0;
+	}
+	
+	public int getNArchi() {
+		if(grafo != null)
+			return grafo.edgeSet().size();
+		
+		return 0;
+	}
+	
+	public List<Airport> trovaPercorso(Airport a1, Airport a2){
+		List<Airport> percorso = new LinkedList<>();
+		
+		BreadthFirstIterator<Airport, DefaultWeightedEdge> it 
+								= new BreadthFirstIterator<>(grafo, a1);
+		
+		
+		visita = new HashMap<>();
+		visita.put(a1, null);
+		
+		it.addTraversalListener(new TraversalListener<Airport, DefaultWeightedEdge>(){
+
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultWeightedEdge> e) {
+				Airport airport1 = grafo.getEdgeSource(e.getEdge());
+				Airport airport2 = grafo.getEdgeTarget(e.getEdge());
+				
+				if(visita.containsKey(airport1) && !visita.containsKey(airport2)) {
+					visita.put(airport2, airport1);
+				} else if (visita.containsKey(airport2) && !visita.containsKey(airport1)){
+					visita.put(airport1, airport2);
+				}
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Airport> e) {
+				
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Airport> e) {
+				
+			}
+
+				
+		});
+		
+		//visito il grafo
+		while(it.hasNext()) {
+			it.next();
+		}
+		
+		
+		//ottengo il percorso dall'albero di visita
+		
+		//se uno dei due aeroporti non è presente nell'albero di visita
+		//   -> non c'è nessun percorso
+		if(!visita.containsKey(a1) || !visita.containsKey(a2)) {
+			return null;
+		}
+		
+		//altrimenti, parto dal fondo e "risalgo" l'albero
+		percorso.add(a2);
+		Airport step = a2;
+		
+		while (visita.get(step) != null) {
+			step = visita.get(step);
+			percorso.add(0,step);
+		}
+		
+		return percorso;
+	}
+	
 }
